541f70abb630a7ee2be95dc03d71fb7680625829
NathallyAlmeida <55113496+NathallyAlmeida@users.noreply.github.com>
2019-11-07 19:47:53 -0300

    Projeto Java
    Desafio (Teste)

---
+++ AutomatedCheckingMachineTeste.java
@@ -0,0 +1,29 @@
+package br.usp.ime.maratona.warmup;
+
+import br.usp.ime.maratona.warmup.solutions.AutomatedCheckingMachine;
+
+/**
+ *
+ * @author Thiago Nascimento <nascimenthiago@gmail.com>
+ */
+public class AutomatedCheckingMachineTeste extends DesafioTeste<String> {
+    
+    static {
+        _instance = new AutomatedCheckingMachineTeste();
+    }
+    
+    @Override
+    public String getInputDirectory() {
+        return "./maratona-input/AutomatedCheckingMachine/";
+    }
+
+    @Override
+    public String getOutputDirectory() {
+        return "./maratona-input/AutomatedCheckingMachine/";
+    }
+
+    @Override
+    public Desafio<String> getDesafio() {
+        return new AutomatedCheckingMachine();
+    }
+}
